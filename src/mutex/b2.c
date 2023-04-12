fba1d49a27cdae76fadf90e5c22d0b293ef9a193
Mateus Costa <info.mateuscosta@gmail.com>
2023-04-12 10:22:00 -0300

    trylock

---
+++ src/mutex/b2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+pthread_t t1;
+pthread_t t2;
+
+pthread_mutex_t mutex_saldo = PTHREAD_MUTEX_INITIALIZER;
+
+double balance = 10000.0;
+
+void worker_code_1(void) {
+    double old_balance, new_balance;
+    
+    sleep(1);
+
+    for (int n=0; n < 100; ++n) {
+        while (pthread_mutex_trylock(&mutex_saldo) != 0)
+            printf("Subtractor: global resource is locked.\n");
+
+        old_balance = balance;
+        new_balance = old_balance - 10;
+        printf("Subtractor: Balance went from: %.2f to %.2f\n", old_balance, new_balance);
+
+        balance = new_balance;
+        pthread_mutex_unlock(&mutex_saldo);
+    }
+}
+
+void worker_code_2(void) {
+    double old_balance, new_balance;
+    
+    sleep(1);
+
+    for (int n=0; n < 100; ++n) {
+        while (pthread_mutex_trylock(&mutex_saldo) != 0)
+            printf("Adder: global resource is locked.\n");
+
+        old_balance = balance;
+        new_balance = old_balance + 10;
+        printf("Adder: Balance went from: %.2f to %.2f\n", old_balance, new_balance);
+
+        balance = new_balance;
+        pthread_mutex_unlock(&mutex_saldo);
+    }
+}
+
+int main(void) {
+    printf("Beginning\n");
+
+    pthread_create(&t1, NULL, (void *)worker_code_1, NULL);
+    pthread_create(&t2, NULL, (void *)worker_code_2, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Done: new balance is %.2f\n", balance);
+
+    return(0);
+}
